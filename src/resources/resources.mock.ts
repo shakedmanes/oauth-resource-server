310f2c5e6b138ace250ef3dcdd654c62bfd4190a
Shaked Manes <maneshaked@gmail.com>
2018-10-31 09:38:54 +0200

    fix: Renaming folder name for the resources of the client consumer

--- src/resources/resources.mock.ts
+++ src/resources/resources.mock.ts
@@ -4,13 +4,16 @@ import config from '../config';
 
 // Mock resources for files associated to end users and servers
 export const resources: { [resourceOwner: string]: { filesPath: string }; } = {
+  // End-users
   '5b829d9efb6fc0183b412bdf': {
     filesPath: config.resourcesPath('5b829d9efb6fc0183b412bdf'),
   },
   '5bd1af6cfb6fc017d2e6404a': {
     filesPath: config.resourcesPath('5bd1af6cfb6fc017d2e6404a'),
   },
-  OAUTH_CONSUMER_CLIENT_ID: {
-    filesPath: config.resourcesPath(''),
+
+  // Clients
+  '5bd6b6afe00fc23b8c2ea7a7': {
+    filesPath: config.resourcesPath('5bd6b6afe00fc23b8c2ea7a7'),
   },
 };
